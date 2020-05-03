02b9db5428ed673a441bb324f1e99391e4931e5d
Sarah German <sgerman@pantheon.io>
2020-05-03 12:18:57 -0500

    Add SQL queries.

---
+++ sql/slide2.sql
@@ -0,0 +1,21 @@
+/**
+ * Find the name of the staff member in each store 
+ * with the largest amount of sales.
+ */
+WITH t1 AS (
+    SELECT CONCAT(s.first_name, ' ', s.last_name) AS name, s.store_id, SUM(p.amount) total_amt
+    FROM staff s
+    JOIN payment p
+    ON p.staff_id = s.staff_id
+    GROUP BY 1, 2
+    ORDER BY 3 DESC
+),
+t2 AS (
+    SELECT store_id, MAX(total_amt) total_amt
+    FROM t1
+    GROUP BY 1
+)
+SELECT t1.name, t1.store_id, t1.total_amt
+FROM t1
+JOIN t2
+ON t1.store_id = t2.store_id AND t1.total_amt = t2.total_amt;
---
+++ sql/slide4.sql
@@ -0,0 +1,15 @@
+/**
+ * What is the lifetime average amount spent 
+ * for the top 10 total spending customers?
+ */
+WITH t1 AS (
+   SELECT c.customer_id, CONCAT(c.first_name, ' ', c.last_name) AS name, SUM(p.amount) tot_spent
+   FROM customer c
+   JOIN payment p
+   ON p.customer_id = c.customer_id
+   GROUP BY c.customer_id, name
+   ORDER BY 3 DESC
+   LIMIT 10)
+
+SELECT ROUND(AVG(tot_spent), 2)
+FROM t1;

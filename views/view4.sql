eef87aa951d111fad5633c83048f4f7965d1b5e1
Sarah German <sgerman@pantheon.io>
2020-05-03 14:55:35 -0500

    Create views for our demo queries.

---
+++ views/view4.sql
@@ -0,0 +1,18 @@
+/**
+ * What is the lifetime average amount spent 
+ * for the top 10 total spending customers?
+ */
+
+CREATE VIEW s4_average_spending_top10_customers AS (
+   WITH t1 AS (
+      SELECT c.customer_id, CONCAT(c.first_name, ' ', c.last_name) AS name, SUM(p.amount) tot_spent
+      FROM customer c
+      JOIN payment p
+      ON p.customer_id = c.customer_id
+      GROUP BY c.customer_id, name
+      ORDER BY 3 DESC
+      LIMIT 10)
+
+   SELECT ROUND(AVG(tot_spent), 2)
+   FROM t1
+);

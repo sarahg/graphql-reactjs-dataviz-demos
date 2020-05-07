6e4b8bc460ff2d2265b6c9de3d308a822e6a0e89
Sarah German <sgerman@pantheon.io>
2020-05-07 08:50:28 -0500

    Security update for Hasura

--- docker-run.sh
+++ docker-run.sh
@@ -2,4 +2,4 @@
 docker run -d -p 8080:8080 \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://sarahgerman:@host.docker.internal:5432/dvdrental \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-       hasura/graphql-engine:v1.2.0
+       hasura/graphql-engine:v1.2.1

c3be9767bfc57f7df131372a6fdce742d1826101
Sarah German <sgerman@pantheon.io>
2020-05-08 20:01:41 -0500

    Rewrite query 4 for a better visualization

--- sql/slide4.sql
+++ sql/slide4.sql
@@ -1,12 +1,14 @@
 /* Query 4 */
 WITH t1 AS (
-   SELECT c.customer_id, CONCAT(c.first_name, ' ', c.last_name) AS name, SUM(p.amount) tot_spent
-   FROM customer c
-   JOIN payment p
-   ON p.customer_id = c.customer_id
-   GROUP BY c.customer_id, name
-   ORDER BY 3 DESC
-   LIMIT 10)
-
-SELECT ROUND(AVG(tot_spent), 2)
-FROM t1;
+    SELECT f.rating, (r.return_date - r.rental_date) rental_length
+    FROM rental r
+    JOIN inventory i
+    ON i.inventory_id = r.inventory_id
+    JOIN film f
+    ON f.film_id = i.inventory_id
+)
+SELECT rating, COUNT(rating) rental_count
+FROM t1
+WHERE DATE_PART('day', rental_length) >= 7
+GROUP BY rating
+ORDER BY rental_count DESC;
